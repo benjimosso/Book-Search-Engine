e2367c7657291e8e7f64c5fb8cde30753fb8e6e6
benjimosso <benjimosso@gmail.com>
2021-08-29 15:16:17 -0700

    TypeDefs Starting

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+console.log(User)
+const resolvers = {
+    
+
+}
+
+
+module.exports = resolvers;
